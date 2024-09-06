595ba9861701d0c33c2970db9612e23caf260f24
SweetDavee <petrovic.david.ns@gmail.com>
2024-09-06 22:24:27 +0200

    factories-seeders

--- pulsews/database/seeders/DatabaseSeeder.php
+++ pulsews/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::truncate();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(5)
+            ->has(Customer::factory(3)
+                ->has(Order::factory(3)
+                    ->has(Product::factory(4))))
+            ->create();
     }
 }

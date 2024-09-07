aeeca4d2a8293b230ab349ea9d90056ff9199a23
SweetDavee <petrovic.david.ns@gmail.com>
2024-09-07 12:45:01 +0200

    app-alijasi

--- pulsews/config/app.php
+++ pulsews/config/app.php
@@ -178,3 +178,20 @@ return [
         App\Providers\RouteServiceProvider::class,
 
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Class Aliases
+    |--------------------------------------------------------------------------
+    |
+    | This array of class aliases will be registered when this application
+    | is started. However, feel free to register as many as you wish as
+    | the aliases are "lazy" loaded so they don't hinder performance.
+    |
+    */
+
+    'aliases' => Facade::defaultAliases()->merge([
+        'CSV' => Maatwebsite\Excel\Facades\Excel::class,
+    ])->toArray(),
+
+];
